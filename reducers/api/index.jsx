9394c0e71e3850cee312f2bd75290d1996c32eff
Rasmus Eskola <fruitiex@gmail.com>
2016-08-04 14:33:10 +0300

    WIP: login/logout with actions

--- reducers/api/index.jsx
+++ reducers/api/index.jsx
@@ -2,6 +2,7 @@ import 'isomorphic-fetch';
 import reduxApi, {transformers} from 'redux-api';
 import adapterFetch from 'redux-api/lib/adapters/fetch';
 import { Map } from 'immutable';
+import { logOut } from '../../actions/ui';
 
 const PORT = 3001;
 const API_ROOT = window.location.protocol + '//' + window.location.hostname + ':' + PORT;
@@ -10,25 +11,33 @@ export default reduxApi({
   auth: {
     url: `${API_ROOT}/employees/authenticate/`,
     transformer(data) {
+      let authSession = localStorage.getItem('auth')
       if (data) {
         console.log('got data');
         console.log(data);
         const { token, employeeId } = data;
         return Map({ auth: true, token, employeeId });
         localStorage.setItem('auth', { token, employeeId });
-      } else {
-        const { token, employeeId } = localStorage.getItem('auth');
+      } else if (authSession) {
+        const { token, employeeId } = authSession;
         console.log(token, employeeId);
-
-        if (token) {
-          return Map({ auth: true, token, employeeId });
-        } else {
-          return Map({ auth: false, token: '', employeeId: '' });
-        }
+        return Map({ auth: true, token, employeeId });
+      } else {
+        console.log('no auth session');
+        return Map({ auth: false, token: '', employeeId: '' });
       }
     },
     options: {
       method: 'post'
+    },
+    reducer(state, action) {
+      console.log(action);
+      console.log(logOut());
+      console.log(state);
+      if (action.type === logOut().type) {
+        return { ...state, data: Map({ auth: false, token: '', employeeId: '' })};
+      }
+      return state;
     }
   },
   users: {
@@ -39,6 +48,7 @@ export default reduxApi({
       },
       function ({getState}, cb) {
         const { user: { data: { auth }}} = getState();
+        console.log(auth);
 
         auth ? cb() : cb(new Error("Unauthorized"));
       }
